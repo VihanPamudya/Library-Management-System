9349b596f2d8b485d3fde7391752db5db7f8c116
vihan pamudya <vihanpamudya12@gmail.com>
2022-03-11 00:55:52 -0800

    Implemented server file

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import 'dotenv/config';
+
+const app = express();
+const PORT = process.env.PORT || "8090";
+
+app.use(cors());
+app.use(express.json({limit: "20mb"}))
+
+app.get('/',(req,res,next)=>{
+    res.send("<h2>Library Mnagement System</h2>");
+    next();
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on PORT ${PORT}`)
+})
