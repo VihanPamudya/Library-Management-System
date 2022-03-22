71a278311e20627c9622fb38c0e5f5c51b962746
vihan pamudya <vihanpamudya12@gmail.com>
2022-03-23 00:25:58 +0530

    added session secret

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import passport from "passport";
+import session from "express-session";
+import config from "./configs";
 import logger from "./utils/logger";
 import connect from "./utils/databaseConnection";
 import "dotenv/config";
@@ -11,6 +13,11 @@ const PORT = process.env.PORT || "8090";
 
 app.use(cors());
 app.use(express.json({ limit: "20mb" }));
+app.use(session({
+  
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.get("/", (req, res, next) => {
   res.send("<h2>Library Mnagement System</h2>");
--- src/configs/index.js
+++ src/configs/index.js
@@ -6,6 +6,7 @@ const config = {
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
   GOOGLE_REDIRECT_URL: process.env.GOOGLE_REDIRECT_URL,
+  SESSION_SECRET: process.env.SESSION_SECRET
 };
 
 export default config;
